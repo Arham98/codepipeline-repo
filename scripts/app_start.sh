07c40cdff5ddfb6e0e2766cc8a8bd083036e6bac
Arham Saeed <saeedarham100@gmail.com>
2023-03-29 01:05:36 +0500

    node permission fix

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Providing complete permissions to the get-files folder
-sudo chmod -R 777 /home/ec2-user/get-files
+sudo chmod -R 777 /home/ec2-user/get-files /web-app
 
 # Navigating to the web application folder
 cd /home/ec2-user/get-files/web-app
 
 # Installing node modules for the web application
-npm install
+sudo npm install
 
 # Starting web application in the background
-node web-app/index.js > app.out.log 2> app.err.log < /dev/null & 
+sudo node web-app/index.js > app.out.log 2> app.err.log < /dev/null & 

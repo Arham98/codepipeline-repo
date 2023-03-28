0e2abad36fc00548fac0b2fdfc0145c698beb4e7
Arham Saeed <saeedarham100@gmail.com>
2023-03-29 04:45:15 +0500

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-
-# Enabling admin access
-sudo su
-
-# Providing complete permissions to the get-files folder
-sudo chmod -R 777 /home/ec2-user/git-files/web-app
-
-mkdir test
-# Navigating to the web application folder
+#!/bin/bash -xe
+source /home/ec2-user/.bash_profile
 cd /home/ec2-user/git-files/web-app
-
-mkdir test2
-# Installing node modules for the web application
 npm install
+mkdir test
+# sudo su
+# cd /home/ec2-user/git-files/web-app
+# npm run start
 
-# Starting web application in the background
-nohup node index.js > /dev/null 2>&1 & 
+# # Enabling admin access
+# sudo su
+# 
+# # Providing complete permissions to the get-files folder
+# sudo chmod -R 777 /home/ec2-user/git-files/web-app
+# 
+# mkdir test
+# # Navigating to the web application folder
+# cd /home/ec2-user/git-files/web-app
+# 
+# mkdir test2
+# # Installing node modules for the web application
+# npm install
+# 
+# # Starting web application in the background
+# nohup node index.js > /dev/null 2>&1 & 

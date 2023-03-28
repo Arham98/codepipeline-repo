0623669cb5db789e4baa2d102d12889f792fddb6
Arham Saeed <saeedarham100@gmail.com>
2023-03-29 02:32:00 +0500

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,11 +3,11 @@
 # Providing complete permissions to the get-files folder
 sudo chmod -R 777 /home/ec2-user/git-files/web-app
 
-# Navigating to the web application folder
-cd /home/ec2-user/git-files/web-app
-
 # Enabling admin access
 sudo su
+
+# Navigating to the web application folder
+cd /home/ec2-user/git-files/web-app
 
 # Installing node modules for the web application
 npm install

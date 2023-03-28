083fca98394cc8b156812c25e0f059192b8dd4c9
Arham Saeed <saeedarham100@gmail.com>
2023-03-29 03:55:51 +0500

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Providing complete permissions to the get-files folder
-sudo chmod -R 777 /home/ec2-user/git-files/web-app
+sudo chmod -R 777 /home/ec2-user/git-files/web-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 # Enabling admin access
-sudo su
+sudo su >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 # Navigating to the web application folder
-cd /home/ec2-user/git-files/web-app
+cd /home/ec2-user/git-files/web-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 # Installing node modules for the web application
-npm install
+npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 # Starting web application in the background
-nohup node index.js > /dev/null 2>&1 & 
+nohup node index.js > /dev/null 2>&1 &  >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
